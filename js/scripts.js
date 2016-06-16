2ceba5ed770ee4ab9552ebc38dd95b423c3017ef
Quinton Williams and Rick Thornbrugh <quinton+rick@>
2016-06-16 14:26:30 -0700

    corrected features on dating website

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function(event) {
   $("form#celebrity").submit(function(event) {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
 
-    var celebrity1 = ("Scarlet Johansen");
-    var celebrity2 = ("Melissa MCarthy")
-    if (gender === 'male' && age < 30) {
-      celebrity1;
-    }
 
-    $("#name").empty().append(hidden);
-    $("#celebrity1").show();
+    //var celebrity2 = ("Melissa MCarthy")
+    if (gender === 'male' && age < 30) {
+      var celebrity = ("Scarlet Johansen");
+  }     else if (gender === 'male' && (age > 30 && age < 70)) {
+         var celebrity = ("Angela Basset ");
+  }     else {
+        var celebrity = ("Betty White");
+  }
+
+
+    $("#name").empty().append(celebrity);
+    $("#hidden").show();
 
     event.preventDefault();
   });
